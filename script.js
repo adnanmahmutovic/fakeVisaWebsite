d67248db09eb4ae47ff75cdbb1294999ac92f4f3
adnanmahmutovic <mahmutovic2025@gmail.com>
2024-10-06 22:42:07 -0500

    Slideshow Background Implemented

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// array of background images
+const images = [
+    'stockVisa.jpg', 
+    'happyVisa.jpg',
+    'headquartersVisa.jpg',
+    'corporateVisa.jpg',
+    'nycVisa.jpg'
+];
+
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+    const slideshowElement = document.getElementById('slideshow');
+    slideshowElement.style.backgroundImage = `url(${images[currentImageIndex]})`;
+    
+    // incrementing the index to the next image, looping back when at the end
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+// ensuring that first image immediately loads when the page opens
+window.onload = function() {
+    changeBackgroundImage(); // setting the initial image
+    setInterval(changeBackgroundImage, 5000); // starting the slideshow interval, changing the image every 5 seconds
+};
+
 function compareCard() {
     const spending = document.getElementById('spending').value;
     const result = document.getElementById('result');
