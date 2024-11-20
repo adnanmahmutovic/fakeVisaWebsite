06e0922fc60e3e5f1eef131370ab681c23b3042e
adnanmahmutovic <mahmutovic2025@gmail.com>
2024-11-20 13:16:08 -0600

    Changed website name and style - also added Our Story page

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 // array of background images
 const images = [
-    'stockVisa.jpg', 
+    'stock.jpg', 
     'happyVisa.jpg',
-    'headquartersVisa.jpg',
-    'corporateVisa.jpg',
-    'nycVisa.jpg'
+    'trading.jpg',
+    'nycVisa.jpg',
+    'desk.jpg',
+    'usingCard.jpg',
 ];
 
 let currentImageIndex = 0;
 
 function changeBackgroundImage() {
     const slideshowElement = document.getElementById('slideshow');
-    slideshowElement.style.backgroundImage = `url(${images[currentImageIndex]})`;
+    const imageUrl = `url(${images[currentImageIndex]})`;
+    console.log("Setting background image:", imageUrl); // Debugging line
+    slideshowElement.style.backgroundImage = imageUrl;
     
     // incrementing the index to the next image, looping back when at the end
     currentImageIndex = (currentImageIndex + 1) % images.length;
 }
 
+function preloadImages(imageArray) {
+    imageArray.forEach((image) => {
+        const img = new Image();
+        img.src = image;
+    });
+}
+
 // ensuring that first image immediately loads when the page opens
 window.onload = function() {
-    changeBackgroundImage(); // setting the initial image
-    setInterval(changeBackgroundImage, 5000); // starting the slideshow interval, changing the image every 5 seconds
+    preloadImages(images); // Preload all images
+    changeBackgroundImage(); // Setting the initial image
+    setInterval(changeBackgroundImage, 5000); // Starting the slideshow interval, changing the image every 5 seconds
 };
 
 function compareCard() {
@@ -29,14 +40,8 @@ function compareCard() {
     let recommendation;
 
     if (spending < 500) {
-        recommendation = "The Visa Bronze Card is perfect for you! It offers 1% cashback on all purchases.";
+        recommendation = "The MVM Bronze Card is perfect for you! It offers 1% cashback on all purchases.";
     } else if (spending < 2000) {
-        recommendation = "You should go for the Visa Silver Card! It offers 1.5% cashback and travel benefits.";
+        recommendation = "You should go for the MVM Silver Card! It offers 1.5% cashback and travel benefits.";
     } else if (spending >= 2000) {
-        recommendation = "The Visa Gold Card is the best fit for you! It provides 2% cashback and premium rewards.";
-    } else {
-        recommendation = "Please enter a valid spending amount.";
-    }
-
-    result.textContent = recommendation;
-}
+        recommendation = "The MVM Gold Card is the best
